55144f54bb49067702ac3406873b330f89da0d4d
PaesaIssFine <isabella.rmaddox@gmail.com>
2024-08-02 16:23:00 -0400

    Initial Commit

---
+++ src/main/java/net/rybloom/mccourse/MCCourseModClient.java
@@ -0,0 +1,10 @@
+package net.rybloom.mccourse;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MCCourseModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
